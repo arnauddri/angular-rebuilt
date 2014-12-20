6457d265b96d71065d9f655807ded54fa9885172
Arnaud Drizard <arnaud.drizard@gmail.com>
2014-12-20 17:44:50 +0100

    4watch comparing values

--- src/scope.js
+++ src/scope.js
@@ -1,6 +1,36 @@
 /* jshint globalstrict: true */
 'use strict';
+var _ = require('lodash')
 
-module.exports = function Scope() {
+module.exports = Scope;
+
+function Scope() {
+  if (!(this instanceof Scope))
+    return new Scope();
+
+  this.$$watchers = [];
 }
 
+Scope.prototype.$watch = function(watchFn, listenerFn) {
+  var watcher = {
+    watchFn: watchFn,
+    listenerFn: listenerFn
+  };
+
+  this.$$watchers.push(watcher);
+}
+
+Scope.prototype.$digest = function() {
+  var self = this;
+  var newValue, oldValue;
+
+  _.forEach(this.$$watchers, function(watcher) {
+    newValue = watcher.watchFn(self);
+    oldValue = watcher.last;
+
+    if (newValue !== oldValue) {
+      watcher.last = newValue;
+      watcher.listenerFn(newValue, oldValue, self);
+    }
+  })
+};
--- test/scope_spec.js
+++ test/scope_spec.js
@@ -11,7 +11,7 @@ describe('Scope', function() {
 
   it('can be constructed as an object', function() {
     var scope = new Scope();
-    scope.aPrperty = 1;
+    scope.aProperty = 1;
 
     expect(scope.aProperty).to.equal(1)
   })
@@ -26,12 +26,45 @@ describe('Scope', function() {
     it('calls the listener function of a watch on first $digest', function() {
       var watchFn = function() { return 'wat'; }
       var listenerFn = sinon.spy();
-
       scope.$watch(watchFn, listenerFn);
 
       scope.$digest();
 
-      expect(sinon.called).to.be.true
+      expect(listenerFn.called).to.be.true
+    })
+
+    it('calls the watch function with the scope as the argument', function() {
+      var watchFn = sinon.spy();
+      var listenerFn = function() {};
+      scope.$watch(watchFn, listenerFn);
+
+      scope.$digest();
+
+      expect(watchFn.called).to.be.true
+    })
+
+    it('calls the listener function when the watched value cahnges', function() {
+      scope.someValue = 'a';
+      scope.counter = 0;
+
+      scope.$watch(
+        function(scope) { return scope.someValue; },
+        function(newValue, oldValue, scope) { scope.counter++; }
+      )
+
+      expect(scope.counter).to.equal(0)
+
+      scope.$digest();
+      expect(scope.counter).to.equal(1)
+
+      scope.$digest();
+      expect(scope.counter).to.equal(1)
+
+      scope.someValue = 'b';
+      expect(scope.counter).to.equal(1)
+
+      scope.$digest();
+      expect(scope.counter).to.equal(2)
     })
   });
 
